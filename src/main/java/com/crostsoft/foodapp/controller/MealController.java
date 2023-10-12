18fd81e1960be90a1fa3625eb49d2aedd3e4ae02
wilfriedpapt89 <wilfriedpapt@gmail.com>
2023-10-12 16:43:20 +0200

    adding CrossOrigin policies

--- src/main/java/com/crostsoft/foodapp/controller/MealController.java
+++ src/main/java/com/crostsoft/foodapp/controller/MealController.java
@@ -5,15 +5,14 @@ import com.crostsoft.foodapp.service.MealService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/meals")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE})
 public class MealController {
 
 
